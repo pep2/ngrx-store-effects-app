9e7762dfb4b3291d532555680873a4e1cd21dadf
Holly Pepper <holly.pepper@nccgroup.com>
2019-02-04 15:47:00 +0000

    done

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,4 +1,4 @@
-import {ActionReducerMap} from "@ngrx/store";
+import {ActionReducerMap, createFeatureSelector, createSelector} from "@ngrx/store";
 import * as fromPizzas from './pizzas.reducer';
 
 export interface ProductsState {
@@ -8,3 +8,17 @@ export interface ProductsState {
 export const reducers: ActionReducerMap<ProductsState> = {
   pizzas: fromPizzas.reducer
 };
+
+export const getProductState = createFeatureSelector<ProductsState>(
+  'products'
+);
+
+// pizza state
+export const getPizzaState = createSelector(
+  getProductState,
+  (state: ProductsState) => state.pizzas // when we're inside here (state.pizzas) where inside the pizzas reducer
+);
+
+export const getAllPizzas = createSelector(getPizzaState, fromPizzas.getPizzas);
+export const getPizzasLoading = createSelector(getPizzaState, fromPizzas.getPizzasLoading);
+export const getPizzasLoaded = createSelector(getPizzaState, fromPizzas.getPizzasLoaded);
